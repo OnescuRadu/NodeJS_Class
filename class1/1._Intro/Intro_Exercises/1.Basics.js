73d5f8bb10c0770d46a73bc223d85b7b681f82e0
OnescuRadu <onescuradu@yahoo.com>
2020-02-06 09:51:34 +0100

    Added the first exercises

---
+++ class1/1._Intro/Intro_Exercises/1.Basics.js
@@ -0,0 +1,9 @@
+var firstName = 'Radu';
+var lastName = 'Onescu';
+
+console.log(`My first name is ${firstName} and my last name is ${lastName}.`);
+
+var year = '2017';
+var number = 1;
+
+console.log(Number(year) + number);
